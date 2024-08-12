9a643050654c6ff8573950a042628dd401a7e51b
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-08-12 15:54:55 -0400

    Add children for bottom nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 12 Aug 2024, 1:13:01 PM
- *  Last update: 12 Aug 2024, 3:13:55 PM
+ *  Last update: 12 Aug 2024, 3:53:42 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import styles from './App.module.css';
@@ -10,8 +10,9 @@ import BottomNav from './components/BottomNav';
 export default function App() {
     return (
         <>
-            <h1 className={styles.headerWhite}>Stardew Calculator - Web</h1>
-            <BottomNav />
+            <BottomNav>
+                <h1 className={styles.headerWhite}>Stardew Calculator - Web</h1>
+            </BottomNav>
         </>
     );
 }
--- src/components/BottomNav/index.jsx
+++ src/components/BottomNav/index.jsx
@@ -1,35 +1,39 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 12 Aug 2024, 2:49:09 PM
- *  Last update: 12 Aug 2024, 3:27:35 PM
+ *  Last update: 12 Aug 2024, 3:51:59 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import styles from "./styles.module.css";
 
-export default function BottomNav() {
+export default function BottomNav({ children }) {
     const tabs = ["Inventory", "Home", "Calculator"];
 
     return (
         <div className={styles.container}>
-            {tabs.map((tabName, index) => {
-                const label = tabName;
+            <div className={styles.contentContainer}>
+                {children}
+            </div>
+            <div className={styles.tabContainer}>
+                {tabs.map((tabName, index) => {
+                    const label = tabName;
 
-                // on pressing the tab
-                const onPress = () => {
-                    console.log(`Tab ${tabName} pressed`);
-                };
+                    // on pressing the tab
+                    const onPress = () => {
+                        console.log(`Tab ${tabName} pressed`);
+                    };
 
-                return (
-                    <button
-                        key={index}
-                        onClick={onPress}
-                    >
-                        <p className={styles.label}>
+                    return (
+                        <button
+                            key={index}
+                            onClick={onPress}
+                            className={styles.tabBtn}
+                        >
                             {label}
-                        </p>
-                    </button>
-                );
-            })}
+                        </button>
+                    );
+                })}
+            </div>
         </div>
     );
 }
