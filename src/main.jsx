0cb269dad9f3217890932b564305df5712eadfa3
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-08-12 13:24:05 -0400

    Clean skeleton

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
+/*
+ *  Author: Kaleb Jubar
+ *  Created: 12 Aug 2024, 1:13:01 PM
+ *  Last update: 12 Aug 2024, 1:23:03 PM
+ *  Copyright (c) 2024 Kaleb Jubar
+ */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import App from './App.jsx'
+
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <App />
+    </StrictMode>,
+);
