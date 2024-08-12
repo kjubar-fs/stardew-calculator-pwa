5ae0a3927c37bb7bc9304d9cbd5c0b9104b7a423
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-08-12 16:19:37 -0400

    Add basic rendering of bottom nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 12 Aug 2024, 1:13:01 PM
- *  Last update: 12 Aug 2024, 3:53:42 PM
+ *  Last update: 12 Aug 2024, 4:18:39 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
-import styles from './App.module.css';
 import BottomNav from './components/BottomNav';
+import CalculatorScreen from './screens/CalculatorScreen';
+import HomeScreen from './screens/HomeScreen';
+import InventoryScreen from './screens/InventoryScreen';
 
 export default function App() {
     return (
         <>
-            <BottomNav>
-                <h1 className={styles.headerWhite}>Stardew Calculator - Web</h1>
-            </BottomNav>
+            <BottomNav
+                screenComponents={[InventoryScreen, HomeScreen, CalculatorScreen]}
+                tabNames={["Inventory", "Home", "Calculator"]}
+                initialTab="Home"
+            />
         </>
     );
 }
--- src/components/BottomNav/index.jsx
+++ src/components/BottomNav/index.jsx
@@ -1,26 +1,51 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 12 Aug 2024, 2:49:09 PM
- *  Last update: 12 Aug 2024, 3:51:59 PM
+ *  Last update: 12 Aug 2024, 4:15:15 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
+import { useState } from "react";
+
 import styles from "./styles.module.css";
 
-export default function BottomNav({ children }) {
-    const tabs = ["Inventory", "Home", "Calculator"];
+export default function BottomNav({ screenComponents, tabNames, initialTab }) {
+    // validate props
+    if (!screenComponents || !tabNames || screenComponents.length === 0 || tabNames === 0) {
+        throw new Error("Error creating BottomNav: screenComponents and tabNames must be provided.");
+    }
+    if (screenComponents.length !== tabNames.length) {
+        throw new Error("Error creating BottomNav: screenComponents and tabNames must be the same length.");
+    }
+
+    const tabsInitState = [];
+    for (let i = 0; i < screenComponents.length; i++) {
+        tabsInitState.push({
+            component: screenComponents[i],
+            name: tabNames[i],
+            active: tabNames[i] === initialTab ? true : false,
+        });
+    }
+    if (!initialTab) {
+        tabsInitState[0].active = true;
+    }
+    const [tabs, setTabs] = useState(tabsInitState);
+
+    const getActiveTabComponent = () => {
+        return tabs.filter((tab) => tab.active)[0].component;
+    };
 
     return (
         <div className={styles.container}>
             <div className={styles.contentContainer}>
-                {children}
+                {getActiveTabComponent()()}
             </div>
             <div className={styles.tabContainer}>
-                {tabs.map((tabName, index) => {
-                    const label = tabName;
+                {tabs.map((tab, index) => {
+                    const label = tab.name;
 
                     // on pressing the tab
                     const onPress = () => {
-                        console.log(`Tab ${tabName} pressed`);
+                        console.log(`Tab ${label} pressed`);
                     };
 
                     return (
