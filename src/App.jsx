df5c39fab212da0ddae4f313ba3d821eef0c00a7
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-08-12 15:16:03 -0400

    Convert App to use module scoped CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 12 Aug 2024, 1:13:01 PM
- *  Last update: 12 Aug 2024, 1:22:46 PM
+ *  Last update: 12 Aug 2024, 3:13:55 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
-import './App.css'
+import styles from './App.module.css';
+import BottomNav from './components/BottomNav';
 
 export default function App() {
     return (
-        <h1>Stardew Calculator - Web</h1>
+        <>
+            <h1 className={styles.headerWhite}>Stardew Calculator - Web</h1>
+            <BottomNav />
+        </>
     );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+/*
+ *  Author: Kaleb Jubar
+ *  Created: 12 Aug 2024, 1:13:01 PM
+ *  Last update: 
+ *  Copyright (c) 2024 Kaleb Jubar
+ */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    css: {
+        modules: {
+            localsConvention: "camelCaseOnly",
+        },
+    },
+});
