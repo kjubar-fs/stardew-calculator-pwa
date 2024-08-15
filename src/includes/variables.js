7f38fce3871bc3036e8e273e5e03513666df9392
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-08-15 13:29:21 -0400

    Add messaging variables

--- src/includes/variables.js
+++ src/includes/variables.js
@@ -1,7 +1,22 @@
 /*
  *  Author: Kaleb Jubar
- *  Created: 7 Aug 2024, 4:50:40 PM
- *  Last update: 
+ *  Created: 14 Aug 2024, 12:37:36 PM
+ *  Last update: 15 Aug 2024, 12:37:26 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 export const userId = "TDIuTPOwRYiYKU0XCn9S";
+
+// background sync tags
+export const settingsSyncTag = "svc-sync-settings";
+
+// message definitions
+export const messageTypes = Object.freeze({
+    refreshData: 0,
+});
+
+export class ClientMessage {
+    constructor(type, source) {
+        this.type = type;
+        this.source = source;
+    }
+}
