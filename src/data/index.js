85b303f80ef9158df03aba224e5ac21d5ace4f7e
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-08-15 14:04:37 -0400

    Save to IDB and request sync when offline

--- src/data/index.js
+++ src/data/index.js
@@ -1,12 +1,15 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 14 Aug 2024, 12:35:30 PM
- *  Last update: 15 Aug 2024, 9:57:36 AM
+ *  Last update: 15 Aug 2024, 1:48:19 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import * as settings from "./state/settingsSlice";
 
-import { setProfessionActive } from "./firebase/write";
+import * as firebaseDbWrite from "./firebase/write";
+import * as localDbWrite from "./local/write";
+
+import { settingsSyncTag } from "../includes/variables";
 
 /**
  * Set the value of the specified profession setting for the given user.
@@ -33,13 +36,45 @@ export async function setProfessionSetting(userId, profession, value, dispatch) 
     dispatch(updateFunc(value));
 
     // run db update
-    const success = await setProfessionActive(userId, profession, value);
+    // if we're online, use Firebase directly
+    if (navigator.onLine) {
+        const success = await firebaseDbWrite.setProfessionActive(userId, profession, value);
 
-    // revert store if db update failed
-    if (!success) {
-        dispatch(updateFunc(!value));
-        return false;
+        // revert store if db update failed
+        if (!success) {
+            dispatch(updateFunc(!value));
+            return false;
+        }
+
+        return true;
+    } 
+    // in offline mode, save to IDB and queue a sync for when we come online
+    // but only if the service worker is available
+    else if ("serviceWorker" in navigator) {
+        // wrap IDB in a try/catch since it works differently than Firebase functions
+        try {
+            // set in local DB
+            await localDbWrite.setProfessionActive(userId, profession, value);
+        } catch {
+            // revert store if db update failed
+            dispatch(updateFunc(!value));
+            return false;
+        }
+        
+        // request a sync if one hasn't been requested already
+        const reg = await navigator.serviceWorker.ready;
+        if (reg.sync) {
+            const tags = await reg.sync.getTags();
+            if (!tags.includes(settingsSyncTag)) {
+                await reg.sync.register(settingsSyncTag);
+            }
+        }
+
+        return true;
     }
-
-    return true;
+    // if we're offline but have no service worker, just return true to enable state changes
+    // I'm not sure this would ever actually happen, but just in case
+    else {
+        return true;
+    }
 }
