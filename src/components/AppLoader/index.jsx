f01af04dcef598b4f4380686d105f7fd1e2f5e43
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-08-15 13:29:47 -0400

    Listen for message to refresh data from SW

--- src/components/AppLoader/index.jsx
+++ src/components/AppLoader/index.jsx
@@ -1,10 +1,12 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 14 Aug 2024, 12:46:39 PM
- *  Last update: 15 Aug 2024, 9:57:41 AM
+ *  Last update: 15 Aug 2024, 1:13:50 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+import { messageTypes } from "../../includes/variables";
 
 import { useSelector, useDispatch } from "react-redux";
 import { setSettings } from "../../data/state/settingsSlice";
@@ -13,15 +15,50 @@ import { loadUserSettings } from "../../data/firebase/read";
 
 export default function AppLoader() {
     // TODO: replace with loading user ID from local DB or creating
+    // get current user ID and make a dispatch for Redux operations
     const userId = useSelector((state) => state.settings.userId);
     const dispatch = useDispatch();
 
+    /**
+     * Load user settings.
+     */
+    const loadSettings = async () => {
+        // get settings from Firebase
+        const settings = await loadUserSettings(userId);
+
+        // overwrite Redux store with latest data
+        dispatch(setSettings(settings));
+    };
+
     // create a mount effect to load
     useEffect(() => {
-        (async () => {
-            // load settings
-            const settings = await loadUserSettings(userId);
-            dispatch(setSettings(settings));
-        })();
+        if ("serviceWorker" in navigator) {
+            navigator.serviceWorker.addEventListener("message", msgHandler);
+        }
+
+        loadSettings();
+
+        return () => {
+            if ("serviceWorker" in navigator) {
+                navigator.serviceWorker.removeEventListener("message", msgHandler);
+            }
+        };
     }, []);
+
+    // create state, handler, and effect for listening to refresh messages from SW
+    const [message, setMessage] = useState(null);
+    const msgHandler = (event) => {
+        // pull out the data from the message event
+        const msg = event.data;
+        if (msg && ("type" in msg) && msg.type === messageTypes.refreshData) {
+            // message came from our SW and tells us to refresh, so set message to trigger a reload
+            setMessage(msg);
+        }
+    };
+    useEffect(() => {
+        // message changed, reload data
+        loadSettings();
+
+        // TODO: figure out why SettingsModal doesn't rerender when syncing
+    }, [message]);
 }
