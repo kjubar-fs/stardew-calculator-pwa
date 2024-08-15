e71444e4bb186ff0c85dbf10ed8841ac0a777eab
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-08-15 14:04:30 -0400

    Remove testing buttons

--- src/screens/CalculatorScreen/index.jsx
+++ src/screens/CalculatorScreen/index.jsx
@@ -1,49 +1,14 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 12 Aug 2024, 4:16:19 PM
- *  Last update: 15 Aug 2024, 11:37:07 AM
+ *  Last update: 15 Aug 2024, 2:03:46 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
-import { useSelector } from "react-redux";
-
-import { setProfessionActive } from "../../data/local/write";
-
-import ButtonWithIcon from "../../components/ButtonWithIcon";
-
 export default function CalculatorScreen() {
-    const userId = useSelector((state) => state.settings.userId);
-
-    const testSync = async () => {
-        // console.log("Setting profession active");
-        // await setProfessionActive(userId, "tiller", true);
-
-        if ("serviceWorker" in navigator) {
-            const reg = await navigator.serviceWorker.ready;
-            if (reg.sync) {
-                console.log("Setting profession inactive");
-                await setProfessionActive(userId, "tiller", false);
-
-                console.log("Sync available, syncing...");
-                const tags = await reg.sync.getTags();
-                if (tags.includes("test-sync")) {
-                    console.log("Sync already requested.");
-                    return;
-                }
-                
-                await reg.sync.register("test-sync");
-                console.log("Test sync registered!");
-            }
-        }
-    };
-
     return (
         <div className="navContainer">
             <div className="cardWithBorder">
                 <p>Calculator goes here</p>
-                <ButtonWithIcon
-                    onClick={testSync}
-                    caption="Test Sync"
-                />
             </div>
         </div>
     );
