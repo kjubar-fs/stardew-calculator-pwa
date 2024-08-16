64d1a19effc88671ac367ed6a93ae9d376560b0b
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-08-16 11:24:59 -0400

    Add ItemList screen to stack

--- src/screens/InventoryScreen/index.jsx
+++ src/screens/InventoryScreen/index.jsx
@@ -1,23 +1,31 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 12 Aug 2024, 4:16:13 PM
- *  Last update: 16 Aug 2024, 10:57:21 AM
+ *  Last update: 16 Aug 2024, 11:19:38 AM
  *  Copyright (c) 2024 Kaleb Jubar
  */
-import StackNav, { NavScreen } from "../../components/StackNav";
+import StackNav from "../../components/StackNav";
 import CategoryScreen from "../CategoryScreen";
+import ItemListScreen from "../ItemListScreen";
 
 export default function InventoryScreen() {
     return (
         <StackNav screens={[
             // TODO: find a better way to do this than requiring Object.assign()
-            Object.assign(new NavScreen(), {
+            {
                 component: CategoryScreen,
                 name: "Categories",
                 options: {
-                    // hideHeader: true,
+                    hideHeader: true,
                 },
-            }),
+            },
+            {
+                component: ItemListScreen,
+                name: "ItemList",
+                options: {
+                    displayTitle: "Items",
+                },
+            },
         ]} />
     );
 }
