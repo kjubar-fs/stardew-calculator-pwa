ae2b652c9dd8d2755e5b845b86c3d7bc69180364
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-08-15 23:27:07 -0400

    Add test notification button

--- src/screens/CalculatorScreen/index.jsx
+++ src/screens/CalculatorScreen/index.jsx
@@ -1,14 +1,33 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 12 Aug 2024, 4:16:19 PM
- *  Last update: 15 Aug 2024, 2:03:46 PM
+ *  Last update: 15 Aug 2024, 11:03:56 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
+import { useSelector } from "react-redux";
+
+import ButtonWithIcon from "../../components/ButtonWithIcon";
+
 export default function CalculatorScreen() {
+    const notificationEnabled = useSelector((state) => state.settings.deviceFeatures.notification);
+
     return (
         <div className="navContainer">
-            <div className="cardWithBorder">
+            <div className="cardWithBorder" style={{gap: "20px"}}>
                 <p>Calculator goes here</p>
+                {
+                    notificationEnabled &&
+                    <ButtonWithIcon
+                        caption="Send a calculation complete notification"
+                        onClick={() => {
+                            new Notification("Calculation complete!", {
+                                body: "Your crop calculation has finished.",
+                                icon: "/assets/icon-highres.png",
+                                badge: "/assets/icon.highres.png",
+                            })
+                        }}
+                    />
+                }
             </div>
         </div>
     );
