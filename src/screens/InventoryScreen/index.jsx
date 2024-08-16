76e09c7a1175dbfe3c559bc552671963f6a0b878
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-08-16 09:33:41 -0400

    Add basic category screen

--- src/screens/InventoryScreen/index.jsx
+++ src/screens/InventoryScreen/index.jsx
@@ -1,15 +1,27 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 12 Aug 2024, 4:16:13 PM
- *  Last update: 15 Aug 2024, 9:16:46 AM
+ *  Last update: 16 Aug 2024, 9:32:12 AM
  *  Copyright (c) 2024 Kaleb Jubar
  */
+import { useSelector } from "react-redux";
+
+import ButtonWithIcon from "../../components/ButtonWithIcon";
+
+import styles from "./styles.module.css";
+
 export default function InventoryScreen() {
+    const categoryList = useSelector((state) => state.categories);
+
     return (
-        <div className="navContainer">
-            <div className="cardWithBorder">
-                <p>Inventory goes here</p>
-            </div>
+        <div className={`navContainer ${styles.container}`}>
+            {categoryList.map((category) => (
+                <ButtonWithIcon
+                    key={category.id}
+                    onClick={() => console.log(category.displayName)/*navigation.navigate("ItemList", { category })*/}
+                    caption={category.displayName}
+                />
+            ))}
         </div>
     );
 }
