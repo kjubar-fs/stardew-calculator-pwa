284d2c642082374b043258119effa001eb623ba7
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-08-16 11:06:41 -0400

    Add rendering of screens

--- src/components/StackNav/index.jsx
+++ src/components/StackNav/index.jsx
@@ -1,10 +1,12 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 16 Aug 2024, 9:34:05 AM
- *  Last update: 16 Aug 2024, 9:53:11 AM
+ *  Last update: 16 Aug 2024, 11:05:44 AM
  *  Copyright (c) 2024 Kaleb Jubar
  */
-import { useState } from "react";
+import React, { useState } from "react";
+
+import styles from "./styles.module.css";
 
 export default function StackNav({ screens }) {
     // validate props
@@ -14,12 +16,81 @@ export default function StackNav({ screens }) {
 
     const [activeScreens, setActiveScreens] = useState([screens[0]]);
 
+    /**
+     * @returns a NavScreen representing the current screen
+     */
     const getCurrentScreen = () => {
         return activeScreens[activeScreens.length - 1];
     };
 
+    const navigation = {
+        /**
+         * Add a new instance of the specified screen to the stack.
+         * @param {string} screenName screen to navigate to
+         * @param {any} params parameters to pass to screen
+         */
+        navigate: (screenName, params) => {
+            // attempt to find screen
+            const screenToAdd = screens.find((screen) => screen.name === screenName);
+            if (!screenToAdd) {
+                throw new Error(`Cannot navigate: no screen named ${screenName}.`);
+            }
+
+            // push a new screen
+            const newScreens = [...activeScreens];
+            newScreens.push({...screenToAdd, params});
+            setActiveScreens(newScreens);
+        },
+
+        /**
+         * Pop off the top screen from the nav, if we're not at the bottom already.
+         */
+        pop: () => {
+            if (activeScreens.length <= 1) {
+                return;
+            }
+
+            const newScreens = [...activeScreens];
+            newScreens.pop();
+            setActiveScreens(newScreens);
+        },
+
+        popToRoot: () => {
+            if (activeScreens.length <= 1) {
+                return;
+            }
+
+            const newScreens = [screens[0]];
+            setActiveScreens(newScreens);
+        },
+    };
+
     return (
-        <div>Nav goes here</div>
+        <div className={styles.container}>
+            {!getCurrentScreen().hidesHeader() &&
+                <div className={styles.headerContainer}>
+                    <h4 className={styles.title}>
+                        {getCurrentScreen().getTitle()}
+                    </h4>
+                    {//activeScreens.length !== 1 &&
+                        <button
+                            className={styles.backBtn}
+                            onClick={navigation.pop}
+                        >
+                            back
+                        </button>
+                    }
+                </div>
+            }
+            <div className={`scrollable ${styles.container}`}>
+                {React.createElement(getCurrentScreen().component, {
+                    navigation,
+                    route: {
+                        params: getCurrentScreen().params,
+                    },
+                })}
+            </div>
+        </div>
     );
 }
 
@@ -29,6 +100,14 @@ export class NavScreen {
     constructor (component, name) {
         this.component = component;
         this.name = name;
+    }
+
+    getTitle() {
+        return this.options?.displayTitle || this.name;
+    }
+
+    hidesHeader() {
+        return this.options && this.options.hideHeader;
     }
 }
 
