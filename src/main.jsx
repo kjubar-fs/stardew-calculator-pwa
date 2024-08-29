f764befc1b522b683273382d143680b178915a5f
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-08-29 12:12:17 -0400

    Temporarily disable SW

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,20 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 12 Aug 2024, 1:13:01 PM
- *  Last update: 29 Aug 2024, 12:04:05 PM
+ *  Last update: 29 Aug 2024, 12:12:10 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App.jsx";
-import ErrorScreen from "./screens/ErrorScreen/index.jsx";
-import InventoryScreen from "./screens/InventoryScreen/index.jsx";
 
 import "./index.css";
 
 // register the service worker
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("/service-worker.js", { scope: "/", type: "module" });
-}
+// if ("serviceWorker" in navigator) {
+//     navigator.serviceWorker.register("/service-worker.js", { scope: "/", type: "module" });
+// }
 
 // create the React app
 createRoot(document.getElementById("root")).render(
