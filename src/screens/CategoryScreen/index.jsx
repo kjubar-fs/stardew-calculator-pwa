65a9e58195404641427803883af557d0de74eee7
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-08-30 15:10:08 -0400

    Add link to item list page

--- src/screens/CategoryScreen/index.jsx
+++ src/screens/CategoryScreen/index.jsx
@@ -1,26 +1,32 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 5 Aug 2024, 7:42:58 PM
- *  Last update: 16 Aug 2024, 11:10:48 AM
+ *  Last update: 29 Aug 2024, 2:24:29 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { useSelector } from "react-redux";
+
+import { Link } from "react-router-dom";
 
 import ButtonWithIcon from "../../components/ButtonWithIcon";
 
 import styles from "./styles.module.css";
 
-export default function CategoryScreen({ navigation }) {
+export default function CategoryScreen() {
     const categoryList = useSelector((state) => state.categories);
 
     return (
         <div className={`navContainer ${styles.container}`}>
             {categoryList.map((category) => (
-                <ButtonWithIcon
+                <Link
                     key={category.id}
-                    onClick={() => navigation.navigate("ItemList", { category })}
-                    caption={category.displayName}
-                />
+                    to={`${category.id}`}
+                >
+                    <ButtonWithIcon
+                        caption={category.displayName}
+                        className={styles.button}
+                    />
+                </Link>
             ))}
         </div>
     );
