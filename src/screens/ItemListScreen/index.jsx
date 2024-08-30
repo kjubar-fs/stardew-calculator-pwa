326bb802a5e324cc34d216e98c56962bb12598b3
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-08-30 15:09:43 -0400

    Add TopNav and link to item detail page

--- src/screens/ItemListScreen/index.jsx
+++ src/screens/ItemListScreen/index.jsx
@@ -1,28 +1,42 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 16 Aug 2024, 11:08:43 AM
- *  Last update: 16 Aug 2024, 11:22:24 AM
+ *  Last update: 29 Aug 2024, 2:54:51 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { useSelector } from "react-redux";
 
+import { useParams, Link } from "react-router-dom";
+
+import TopNav from "../../components/TopNav";
 import ButtonWithIcon from "../../components/ButtonWithIcon";
 
 import styles from "./styles.module.css";
 
-export default function ItemListScreen({ navigation, route }) {
+export default function ItemListScreen() {
+    const params = useParams();
+
+    const allCategories = useSelector((state) => state.categories);
+    const category = Object.values(allCategories).find((cat) => cat.id === +params.categoryId);
+    
     const allItems = useSelector((state) => state.items);
-    const itemList = Object.values(allItems).filter((item) => item.category === route.params.category.id);
+    const itemList = Object.values(allItems).filter((item) => item.category === +params.categoryId);
 
     return (
-        <div  className={`navContainer ${styles.container}`}>
-            {itemList.map((item) => (
-                <ButtonWithIcon
-                    key={item.id}
-                    onClick={() => navigation.navigate("ItemDetail", { itemName: item.name, id: item.id })}
-                    caption={item.name}
-                />
-            ))}
-        </div>
+        <TopNav showBack={true} title={category.displayName}>
+            <div className={`navContainer ${styles.container}`}>
+                {itemList.map((item) => (
+                    <Link
+                        key={item.id}
+                        to={`${item.id}`}
+                    >
+                        <ButtonWithIcon
+                            caption={item.name}
+                            className={styles.button}
+                        />
+                    </Link>
+                ))}
+            </div>
+        </TopNav>
     );
 }
