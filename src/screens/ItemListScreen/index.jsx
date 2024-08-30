1b6b2c8db561c966616893ce9a29c44245cad1af
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-08-30 15:27:01 -0400

    Add icons on buttons

--- src/screens/ItemListScreen/index.jsx
+++ src/screens/ItemListScreen/index.jsx
@@ -1,7 +1,7 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 16 Aug 2024, 11:08:43 AM
- *  Last update: 29 Aug 2024, 2:54:51 PM
+ *  Last update: 30 Aug 2024, 3:24:06 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { useSelector } from "react-redux";
@@ -33,6 +33,7 @@ export default function ItemListScreen() {
                         <ButtonWithIcon
                             caption={item.name}
                             className={styles.button}
+                            src={`/assets/sprites/${item.texture}/tile${String(item.spriteIndex).padStart(3, "0")}.png`}
                         />
                     </Link>
                 ))}
