b8451381c74006134453135ec5c4c6d40370e9a0
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-09-11 18:43:26 -0400

    Fix import

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 12 Aug 2024, 1:13:01 PM
- *  Last update: 11 Sep 2024, 6:36:54 PM
+ *  Last update: 11 Sep 2024, 6:43:17 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App.jsx";
+import App from "./App";
 
 import "./index.css";
 
