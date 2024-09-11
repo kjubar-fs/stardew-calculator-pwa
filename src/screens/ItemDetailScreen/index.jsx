e2e6ba18959129bd562edaa3010295620183aa47
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-09-11 18:07:37 -0400

    Add styles file and optional recipe data

--- src/screens/ItemDetailScreen/index.jsx
+++ src/screens/ItemDetailScreen/index.jsx
@@ -1,7 +1,7 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 16 Aug 2024, 11:26:01 AM
- *  Last update: 4 Sep 2024, 7:14:39 PM
+ *  Last update: 11 Sep 2024, 5:55:03 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { useSelector } from "react-redux";
@@ -10,18 +10,31 @@ import { useParams } from "react-router-dom";
 
 import TopNav from "../../components/TopNav";
 
+import styles from "./styles.module.css";
+
 export default function ItemDetailScreen() {
     const params = useParams();
 
     const item = useSelector((state) => state.items[params.itemId]);
 
+    // if item is a cooking recipe, fetch the recipe from state
+    let recipe;
+    if (item.category === 9) {
+        recipe = useSelector((state) => state.cookingRecipes[item.id]);
+    }
+
     return (
         <TopNav showBack={true} title={item.name}>
             <div className="navContainer">
                 <div className="cardWithBorder">
-                    <pre style={{ textWrap: "wrap" }}>
+                    <pre className={styles.code}>
                         {JSON.stringify(item, null, 4)}
                     </pre>
+                    {recipe &&
+                        <pre className={styles.code}>
+                            {JSON.stringify(recipe, null, 4)}
+                        </pre>
+                    }
                 </div>
             </div>
         </TopNav>
