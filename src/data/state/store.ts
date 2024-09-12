48c8f4a7c93780280e356b23057673df238d67f6
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-09-12 11:59:55 -0400

    Add more robust Redux typings

--- src/data/state/store.ts
+++ src/data/state/store.ts
@@ -1,17 +1,18 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 7 Aug 2024, 4:17:44 PM
- *  Last update: 11 Sep 2024, 5:54:32 PM
+ *  Last update: 12 Sep 2024, 11:57:12 AM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { configureStore } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
 
 import settingsReducer from "./settingsSlice";
 import itemDataReducer from "./itemDataSlice";
 import categoriesReducer from "./categoriesSlice";
 import cookingRecipesReducer from "./cookingRecipesSlice";
 
-export default configureStore({
+const store = configureStore({
     reducer: {
         settings: settingsReducer,
         items: itemDataReducer,
@@ -19,3 +20,8 @@ export default configureStore({
         cookingRecipes: cookingRecipesReducer,
     },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export default store;
