ee3ad64ba3ee5bb1e62cc7a2698c11126126f09b
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-09-12 11:23:45 -0400

    Add module defs for json files

---
+++ src/typings.d.ts
@@ -0,0 +1,10 @@
+/*
+ *  Author: Kaleb Jubar
+ *  Created: 12 Sep 2024, 11:22:11 AM
+ *  Last update: 12 Sep 2024, 11:22:26 AM
+ *  Copyright (c) 2024 Kaleb Jubar
+ */
+declare module "*.json" {
+    const value: any;
+    export default value;
+}
