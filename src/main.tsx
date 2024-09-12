f1e8106e4059884d83c007e112dae7e20bb147aa
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-09-12 12:37:08 -0400

    Ignore type error about attaching React app

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 12 Aug 2024, 1:13:01 PM
- *  Last update: 11 Sep 2024, 6:49:59 PM
+ *  Last update: 12 Sep 2024, 12:37:02 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { StrictMode } from "react";
@@ -17,6 +17,7 @@ import "./index.css";
 // }
 
 // create the React app
+// @ts-ignore ignore error about Container type, this works fine
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <App />
